ada961df04bcb28523e68c47363acd411aea9e20
JansenChang <1028408511@qq.com>
2019-03-31 03:55:06 +0800

    上传项目文件

--- serviceribbon/src/main/java/com/jansen/serviceribbon/ServiceribbonApplication.java
+++ serviceribbon/src/main/java/com/jansen/serviceribbon/ServiceribbonApplication.java
@@ -1,4 +1,4 @@
-package com.jansne.serviceribbon;
+package com.jansen.serviceribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
